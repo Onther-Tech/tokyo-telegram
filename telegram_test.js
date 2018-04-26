3692614ea68c901b64d9c2a637c6f61ca314bfd7
Ubuntu <ubuntu@ip-172-31-43-216.us-east-2.compute.internal>
2018-04-26 19:37:48 +0000

    error

--- telegram_test.js
+++ telegram_test.js
@@ -1,34 +1,87 @@
 
-
-var token = require('./config.js')
+var token = require('./config.js');
 const TelegramBot = require('node-telegram-bot-api');
+const async = require('async')
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token.getToken() , {polling: true});
+const bot = new TelegramBot(token.getToken(), {polling: true});
+const chatId = '';
+const answer = {
+    "reply_markup": {
+        "keyboard": seriesKB,
+        "one_time_keyboard": true
+    }
+};
+const opts = {
+  reply_markup: JSON.stringify({
+    keyboard: [
+      ['1. 토큰의 추가 발행이 가능한 토큰(Mintable)'],
+      ['2. 특정 블록높이를 기준으로 잔액 분포를 알 수 있는 토큰(Minime)']
+    ]
+  })
+};
+var seriesKB = [];  // The keyboard array
 
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
 
-  if (msg.text == '정순형'){
-    bot.sendMessage(chatId, 'Kevin');
-  }else if (msg.text == '김휘경'){
-    bot.sendMessage(chatId, 'Zoe');
-  }else if (msg.text == '박주형'){
-    bot.sendMessage(chatId, 'Carl');
-  }else if (msg.text == '강주형'){
-    bot.sendMessage(chatId, 'Jace');
-  }else if (msg.text == '황재승'){
-    bot.sendMessage(chatId, 'Jason');
-  }else if(msg.text == '강희정'){
-    bot.sendMessage(chatId, 'Jenny');
-  }else if (msg.text == '신건우'){
-    bot.sendMessage(chatId, 'Thomas');
-  }else if (msg.text == '허상범'){
-    bot.sendMessage(chatId, 'Danny');
-  }else if (msg.text == '온더'){
-    bot.sendMessage(chatId, '짱');
-  }else if (msg.text == '하이'){
-    bot.sendMessage(chatId, '온더 짱');
-  }
+async.waterfall([
+    function(callback) {
+
+        bot.onText(/^\/start/, function(msg, match) {
+
+        bot.sendMessage(msg.chat.id, "프로젝트 이름은 무엇인가요?", answer );
+        bot.onText(/.+/g, function(msg, match) {
+            console.log(match);
+            callback(null, msg);
+          });
+        });
+
+    },function( msg , callback) {
+
+      bot.sendMessage(msg.chat.id, "토큰의 타입을 선택해주세요", opts);
+      bot.onText(/.+/g, function(msg, match) {
+          console.log(match);
+          callback(null, msg);
+      });
+    },function( msg , callback) {
+
+      console.log(msg);
+      bot.sendMessage(msg.chat.id, "토큰의 이름은 무엇인가요?", answer);
+      bot.onText(/.+/g, function(msg, match) {
+          console.log(match);
+          callback(null, msg);
+      });
+    },function( msg , callback) {
+
+      bot.sendMessage(msg.chat.id, "토큰의 심볼은 무엇인가요?", answer);
+      bot.onText(/.+/g, function(msg, match) {
+          console.log(match);
+          callback(null, msg);
+      });
+    },function( msg , callback) {
+
+      bot.sendMessage(msg.chat.id, "토큰의 소수점은 몇 번째 자리수까지 인가요?", answer);
+      bot.onText(/.+/g, function(msg, match) {
+          console.log(match);
+          callback(null, msg);
+      });
+    },function( msg , callback) {
+
+      bot.sendMessage(msg.chat.id, "사용자가 본인의 토큰을 태워 없애는 기능이 필요한가요?", answer);
+      bot.onText(/.+/g, function(msg, match) {
+          console.log(match);
+          callback(null, msg);
+      });
+    },function( msg , callback) {
+
+      bot.sendMessage(msg.chat.id, "관리자가 토큰의 전체 전송을 일시정지하는 기능이 필요한가요?", answer);
+      bot.onText(/.+/g, function(msg, match) {
+          console.log(match);
+          callback(null, msg);
+      });
+    }
+
+
+], function (err, result) {
 
 });
+
